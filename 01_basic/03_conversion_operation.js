b85fb0462357a6e17f4597c9b0047ea796041b17
sanjeev sharma <sanjeevcs44@gmail.com>
2023-08-25 17:15:21 +0530

    conversion of datatype

---
+++ 01_basic/03_conversion_operation.js
@@ -0,0 +1,23 @@
+let score = "33";
+//let score = null; conversion 0
+// let score = undefined; conversion NAN
+// let score = true;  conversion 1
+// let score = false;  conversion 0
+
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // its converting in number
+
+// let notAnNmuber = "33abc";
+// console.log(typeof notAnNmuber);
+// let valueInNumber1 = Number(notAnNmuber); // when we conver in number we have to check real number is coming or not
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+// 1 true ; 0=>false
+// ""=> false
+// "anjeev"=> true
